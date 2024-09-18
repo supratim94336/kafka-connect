21bcd9150ea8611de6c291a1ffc984496e87a348
supratim94336 <supratim.gre2014@yahoo.com>
2024-09-18 11:00:20 +0200

    make it easier

--- postgres/scripts/init.sql
+++ postgres/scripts/init.sql
@@ -1,16 +1,16 @@
+DROP TABLE IF EXISTS recipes;
 CREATE TABLE recipes (
     recipe_id INT NOT NULL,
     recipe_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (recipe_id),
-    UNIQUE (recipe_name)
+    PRIMARY KEY (recipe_id)
 );
 
+DROP TABLE IF EXISTS ingredients;
 CREATE TABLE ingredients (
     ingredient_id INT NOT NULL, 
     ingredient_name VARCHAR(30) NOT NULL,
     ingredient_price INT NOT NULL,
-    PRIMARY KEY (ingredient_id),  
-    UNIQUE (ingredient_name)
+    PRIMARY KEY (ingredient_id)
 );
 
 ALTER TABLE ingredients REPLICA IDENTITY FULL;
